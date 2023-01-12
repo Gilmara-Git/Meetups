78937309dba0c5112686c5cbfce8de68beed31a0
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-01-11 23:05:17 -0500

    feat: working on new meetup page

--- src/pages/NewMeetUps/index.js
+++ src/pages/NewMeetUps/index.js
@@ -1,5 +1,40 @@
+ import { useRef } from 'react';
+ import classes from './NewMeetups.module.css';
+
 const NewMeetups = ()=>{
-    return <div>New Meet Ups</div>
+    const onSubmitHandler = (event)=>{
+        event.preventDefault();
+        console.log('I was clicked')
+    }
+
+    return (
+    <div className={classes.formWrapper}>
+        <h1>Add your Meetup</h1>
+        <div className={classes.form}>           
+            <form onSubmit={onSubmitHandler}>
+                <div>
+                    <label htmlFor="title">Title</label>
+                    <input type='text' id="title" placeholder='Name your meetup'/>
+                </div>
+
+                <div>
+                    <label htmlFor="image">Image</label>
+                    <input type='text' id="image" placeholder='Enter an image address'/>
+                </div>
+                <div>
+                    <label htmlFor="description">Title</label>
+                    <textarea id="description" placeholder='Describe your meetup'/>
+                </div>
+                <div>
+                    <label htmlFor="coord">Coordinates</label>
+                    <input type='text' id="coord" placeholder='Provide latitude and longitude'/>
+                </div>
+                <button>Add your meeting</button>
+            </form>
+        </div>
+
+    </div>
+    )
 };
 
 export default NewMeetups; 
