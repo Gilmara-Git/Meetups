b80b64206b31ded386b1a78f28645b3921e3a846
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-01-12 09:36:26 -0500

    refactor: adjusted data in Dummy data

--- src/Dummy-data/DummyMeetup.js
+++ src/Dummy-data/DummyMeetup.js
@@ -3,25 +3,25 @@ const DUMMY_MEETUPS= [{
     title: " First meetup", 
     image: "https://upload.wikimedia.org/wikipedia/commons/e/e4/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall_%28cropped%29.jpg" , 
     description: "Palace of Westminster from the dome on Methodist Central Hal",
-    location: "51° 29′ 59.61″ N, 0° 7′ 47.81″ W" 
+    location: "London, United Kingdom" 
 }, 
 {   id: Math.ceil(Math.random() * 1000),
     title: "Second meetup", 
     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Cidade_Maravilhosa.jpg/800px-Cidade_Maravilhosa.jpg" , 
     description: "Christ the Redeemer on Corcovado and Sugarloaf Mountain and Botafogo Bay (background)" , 
-    location: "22° 54′ 40″ S, 43° 12′ 20″ W"
+    location: "Rio de Janeiro, Brasil"
 }, 
 { id: Math.ceil(Math.random() * 1000),
     title: "Third meetup", 
     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Prague_%286365119737%29.jpg/1920px-Prague_%286365119737%29.jpg" , 
     description: "Prague - Capital of the Czech Republic", 
-    location: "50° 5′ 15″ N, 14° 25′ 17″ E"
+    location: "Prague, Czech Republic"
 },
 { id: Math.ceil(Math.random() * 1000),
     title: "Forth meetup" ,
     image: "https://upload.wikimedia.org/wikipedia/commons/b/be/KeizersgrachtReguliersgrachtAmsterdam.jpg",
     description: "Amsterdam,the Netherlands at dusk", 
-    location: "52° 22′ 22″ N, 4° 53′ 37″ E"
+    location: "Amsterdam, Netherlands"
 }
 ];
 
